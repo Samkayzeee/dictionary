bd042ced1034dc6b9bc80f6ffeaa0ace1ceab7e2
Samkayzeee <lasisiabdulsamad9@gmail.com>
2023-06-12 22:10:32 +0100

    updated on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
                 <div className="links">
                     <ul>
                         <li> <a href='https://twitter.com/Samkayzee1' target='blank'> <i className="fa-brands fa-twitter"></i> </a> </li>
-                        <li> <a href='https://github.com/Samkayzeee'> <i className="fa-brands fa-github"></i> </a> </li>
-                        <li> <a href='https://www.linkedin.com/in/lasisi-abdulsamad'> <i className="fa-brands fa-linkedin"></i> </a> </li>
+                        <li> <a href='https://github.com/Samkayzeee' target='blank'> <i className="fa-brands fa-github"></i> </a> </li>
+                        <li> <a href='https://www.linkedin.com/in/lasisi-abdulsamad' target='blank'> <i className="fa-brands fa-linkedin"></i> </a> </li>
                     </ul>
                 </div>
 
